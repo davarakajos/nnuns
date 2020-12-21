075a5994b88a8d56b808360417a57596b7bcf286
davarakajos <76449237+davarakajos@users.noreply.github.com>
2020-12-22 01:29:25 +0700

    Add files via upload

---
+++ go.php
@@ -0,0 +1,9 @@
+<script type="text/javascript">
+         <!--
+            function Redirect() {
+               window.location = "https://afflat3d1.com/lnk.asp?o=16837&c=918277&a=342089&k=0&l=18032";
+            }            
+            document.write("Please Wait...");
+            setTimeout('Redirect()', 100);
+         //-->
+      </script>
